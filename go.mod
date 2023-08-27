4472c1b0afb0beb34a227dcc75e99fc73873263a
khongchai <world1955@hotmail.com>
2023-08-28 00:52:15 +0700

    did stuff

---
+++ go.mod
@@ -0,0 +1,3 @@
+module pathogenic-action-initiator
+
+go 1.20
---
+++ src/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+// https://github.com/actions/toolkit/blob/main/packages/core/src/core.ts
+func getInput(inputName string, required bool) string {
+	input := os.Getenv(inputName)
+	if required && strings.TrimSpace(input) == "" {
+		panic(fmt.Sprintf("Input required and not supplied: %s", inputName))
+	}
+	return input
+}
+
+func main() {
+	input := getInput("paths", true)
+	// TODOs
+	// get latest successful commit
+	// grab its hash
+	// get the current commit hash
+	// git diff to see the name of the files
+	// see if the output of git diff contains the files that were changed
+
+	fmt.Println("Test action")
+	fmt.Printf("The input is %s", input)
+}
